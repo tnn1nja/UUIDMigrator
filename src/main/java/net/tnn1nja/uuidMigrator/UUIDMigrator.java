953f479ae749a8fd55b7ef29a465f995b127dd92
tnn1nja <thenonsenseninja@gmail.com>
2025-03-21 13:21:59 +0000

    Attempt to reschedule main thread so bukkit has time to waste doing nothing

--- src/main/java/net/tnn1nja/uuidMigrator/UUIDMigrator.java
+++ src/main/java/net/tnn1nja/uuidMigrator/UUIDMigrator.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Tameable;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
@@ -17,19 +18,34 @@ import java.util.logging.Logger;
 
 public final class UUIDMigrator extends JavaPlugin implements CommandExecutor {
 
+
+    Plugin me;
     Logger log = Bukkit.getLogger();
     UUID ItsAllGud = UUID.fromString("48a71f98-3e23-4e1c-aba9-39bd8a147b71");
     UUID Its4llGud = UUID.fromString("42609fbb-d926-4790-83d3-f034bd20064e");
+    World w = Bukkit.getWorlds().getFirst();
+    int startX = -704;
+    int currentX = startX;
+    int endX = 287;
+    int startZ = -928;
+    int endZ = 319;
+
 
     @Override
     public void onEnable() {
         log.info("[UUIDMigrator] Successfully loaded.");
-        getCommand("migrate").setExecutor(this);
-        getCommand("fullmigrate").setExecutor(this);
+        getCommand("migrate").setExecutor(me);
+        getCommand("fullmigrate").setExecutor(me);
+        me = this;
+    }
+
+    @Override
+    public void onDisable() {
+        log.info("[UUIDMigrator] Disabled");
     }
 
 
-    public void migrateChunk(World w, int x, int z){
+    public void migrateChunk(int x, int z){
 
         if(w.isChunkGenerated(x, z)){
             Chunk c = w.getChunkAt(x, z);
@@ -65,30 +81,38 @@ public final class UUIDMigrator extends JavaPlugin implements CommandExecutor {
 
     }
 
+    public void migrateChunkLine(int x){
+        for(int z = startZ; z<endZ+1; z++){
+            migrateChunk(x, z);
+        }
+    }
+
     public void migrateAllChunks(){
         log.info("World Migration Started.");
-        World w = Bukkit.getWorlds().getFirst();
-        int startX = -704;
-        int startZ = -928;
-        int endX = 287;
-        int endZ = 319;
-        for(int x = startX; x<endX+1; x++){
-            for(int z = startZ; z<endZ+1; z++){
-                migrateChunk(w, x, z);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(me, new Runnable() {
+            @Override
+            public void run() {
+                if(currentX <= endX) {
+                    migrateChunkLine(currentX);
+                    currentX += 1;
+                    log.info("Migrated Chunk Line at x = " + currentX);
+                }else{
+                    log.info("World Migration Completed.");
+                    Bukkit.getScheduler().cancelTasks(me);
+                }
             }
-            if (x % 10 == 0){
-                log.info("Migrated Till x=" + x);
-            }
-        }
-        log.info("World Migration Complete.");
+        }, 0L, 20L);
+
     }
+
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (command.getName().equalsIgnoreCase("migrate")){
             World w = Bukkit.getWorlds().getFirst();
-            migrateChunk(w, Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            migrateChunk(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
             if(w.isChunkGenerated(Integer.parseInt(args[0]), Integer.parseInt(args[1]))) {
                 log.info("Migrated Chunk at " + args[0] + ", " + args[1]);
             }else{
@@ -97,21 +121,10 @@ public final class UUIDMigrator extends JavaPlugin implements CommandExecutor {
         }
 
         if (command.getName().equalsIgnoreCase("fullmigrate")){
-            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-                @Override
-                public void run() {
-                    migrateAllChunks();
-                }
-            }, 0L, 20L);
             migrateAllChunks();
         }
 
         return true;
 
-    }
-
-    @Override
-    public void onDisable() {
-        log.info("[UUIDMigrator] Disabled");
     }
 }
